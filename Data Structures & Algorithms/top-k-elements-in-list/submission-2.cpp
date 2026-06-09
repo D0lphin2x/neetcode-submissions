0e443ffe47e51986c292e17f28cc49cae9f69036
William He <88105554+D0lphin2x@users.noreply.github.com>
2026-06-09 19:54:10 -0400

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // max heap approach, bucket sort
+        unordered_map<int,int> um;
+        priority_queue<int> pq;
+        vector<vector<int>> bucket(nums.size()+1);
+        vector<int> ans;
+        int count = k;
+        for (int i : nums) {
+            um[i]++;
+        }
+        for (auto& [c,v]: um) {
+            bucket[v].push_back(c);
+        }
+        for (int i = nums.size(); i >= 0; i--) {
+            if (!bucket[i].empty()) {
+                for (int i : bucket[i]) {
+                    ans.push_back(i);
+                    count--;
+                    if (count == 0) return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
