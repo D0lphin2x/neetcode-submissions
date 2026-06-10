6ddec789ccab1a90cca1e500c52ab9fea711bfb4
William He <88105554+D0lphin2x@users.noreply.github.com>
2026-06-09 23:24:47 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int sell = 0;
+        int buy = prices[0];
+        for (int i = 1; i < prices.size(); i++) { 
+            if (prices[i] - buy < 0) {
+                buy = prices[i];
+            }
+            else if (prices[i] - buy > ans) {
+                sell = prices[i];
+                ans = sell - buy;
+            }
+        }
+        return ans;
+    }
+};
