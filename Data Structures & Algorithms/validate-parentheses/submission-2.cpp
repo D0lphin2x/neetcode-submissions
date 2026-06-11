aa42164a0ad96162643a3e7491e766c3afcaa37f
William He <88105554+D0lphin2x@users.noreply.github.com>
2026-06-10 22:12:37 -0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() == 0 || s.size() % 2 != 0) return false;
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '{' || s[i] == '[' || s[i] == '(') {
+                st.push(s[i]);
+            }
+            else {
+                if (st.empty()) return false;
+
+                char c = st.top();
+                st.pop();
+
+                if (s[i] == '}' && c != '{') return false;
+                if (s[i] == ')' && c != '(') return false;
+                if (s[i] == ']' && c != '[') return false;
+            }
+        }
+        return st.empty();
+    }
+};
